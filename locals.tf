73f3ccdbb4c409234fd1f5905d5435b209528489
NANDYALA9010 <sivareddy9017@gmail.com>
2026-07-14 17:58:49 +0530

    added

--- locals.tf
+++ locals.tf
@@ -1,17 +1,15 @@
 locals {
   common = {
-    project = var.project
+    project     = var.project
     environment = var.environment
-    Terraform = "true"
-  
-  vpc_ec2_final_tags = merge(
-                 local.common,
-                {
-                 Name = "${var.project}-${var.environment}"
-                },
-              var.vpc_tags
-  )
+    Terraform   = "true"
   }
-    
-  
+
+  vpc_ec2_final_tags = merge(
+    local.common,
+    {
+      Name = "${var.project}-${var.environment}"
+    },
+    var.vpc_tags
+  )
 }
